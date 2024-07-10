436165ffe06b083de0098f71e9238975628e5984
satyamjh07 <138577860+satyamjh07@users.noreply.github.com>
2024-07-10 13:26:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,29 +6,37 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function smoothScroll(event) {
-        event.preventDefault();
-
         const targetId = event.currentTarget.getAttribute('href');
-        const targetPosition = document.querySelector(targetId).offsetTop;
-        const startPosition = window.pageYOffset;
-        const distance = targetPosition - startPosition;
-        const duration = 100;
-        let start = null;
 
-        window.requestAnimationFrame(step);
+        // Check if the link is internal (starts with "#")
+        if (targetId.startsWith('#')) {
+            event.preventDefault();
 
-        function step(timestamp) {
-            if (!start) start = timestamp;
-            const progress = timestamp - start;
-            window.scrollTo(0, easeInOutCubic(progress, startPosition, distance, duration));
-            if (progress < duration) window.requestAnimationFrame(step);
-        }
+            const targetPosition = document.querySelector(targetId).offsetTop;
+            const startPosition = window.pageYOffset;
+            const distance = targetPosition - startPosition;
+            const duration = 100;
+            let start = null;
 
-        function easeInOutCubic(t, b, c, d) {
-            t /= d / 2;
-            if (t < 1) return c / 2 * t * t * t + b;
-            t -= 2;
-            return c / 2 * (t * t * t + 2) + b;
+            window.requestAnimationFrame(step);
+
+            function step(timestamp) {
+                if (!start) start = timestamp;
+                const progress = timestamp - start;
+                window.scrollTo(0, easeInOutCubic(progress, startPosition, distance, duration));
+                if (progress < duration) window.requestAnimationFrame(step);
+            }
+
+            function easeInOutCubic(t, b, c, d) {
+                t /= d / 2;
+                if (t < 1) return c / 2 * t * t * t + b;
+                t -= 2;
+                return c / 2 * (t * t * t + 2) + b;
+            }
+        } else {
+            // Allow default action for external links
+            event.preventDefault();
+            window.open(targetId, '_blank');
         }
     }
 });
